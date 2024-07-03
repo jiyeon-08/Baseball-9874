cc850b858bf22b50b46a7bfbff5d5044f0a91ce5
jiyeon <jiyeon08.you@samsung.com>
2024-07-03 14:57:37 +0900

    [refactoring] 3

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -4,19 +4,30 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		asserIllegalArgument(guessNumber);
+	}
+
+	void asserIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be trhee letters.");
 		}
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("Must be number");
-			}	
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
 		}
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number.");
 		}
 	}
 
+	bool isDuplicatedNumber(const string& guessNumber)
+	{
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2]) {
+			return true;
+		}
+		return false;
+	}
 };
