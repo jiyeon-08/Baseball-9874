ae4780a6a39c98f0f1cbf0bfb9e27aa012c3ab44
jiyeon <jiyeon08.you@samsung.com>
2024-07-03 14:50:53 +0900

    [feature] 3

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -10,8 +10,12 @@ public:
 		for (char ch : guessNumber) {
 			if (ch < '0' || ch > '9') {
 				throw invalid_argument("Must be number");
-			}
-			
+			}	
+		}
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2]) {
+			throw invalid_argument("Must not have the same number.");
 		}
 	}
 
